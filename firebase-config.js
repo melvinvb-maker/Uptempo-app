fdad3e979a640ed33b1aa560ac89f3a4e17f4e6e
melvinvb-maker <melvin.vb@icloud.com>
2026-06-22 14:54:13 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Vul hier jouw Firebase-config in. Laat enabled op false zolang je Firebase nog niet hebt aangemaakt.
+export const firebaseEnabled = false;
+export const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
